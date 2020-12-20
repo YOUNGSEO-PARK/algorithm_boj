21e9c06285ed8a112416c88f37f46305f605ddea
YOUNGSEO-PARK <irene0seo97@gmail.com>
2020-12-20 16:49:18 +0900

    baekjoon - class 1 solved

---
+++ src/bronze/b_3052.java
@@ -0,0 +1,24 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b_3052 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10];
+        int[] ans = new int[42];
+        int sum = 0;
+
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            ans[arr[i]%42] = 1;
+        }
+        for(int k : ans){
+            sum+=k;
+        }
+        System.out.println(sum);
+    }
+}
