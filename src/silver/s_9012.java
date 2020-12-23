ff9dc60f5edeea2c99fd4c0138d8c74035f87d9a
YOUNGSEO-PARK <irene0seo97@gmail.com>
2020-12-23 22:08:02 +0900

    [class 2] #9012 괄호 solved

---
+++ src/silver/s_9012.java
@@ -0,0 +1,33 @@
+package silver;
+
+import java.io.*;
+
+public class s_9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int open = 0, close = 0;
+            String s = br.readLine();
+
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '(') {
+                    open++;
+                } else {
+                    close++;
+                }
+                if (open < close) {
+                    System.out.println("NO");
+                    break;
+                }
+            }
+            if (open == close) {
+                System.out.println("YES");
+            }
+            else if(open > close){
+                System.out.println("NO");
+            }
+        }
+    }
+}
