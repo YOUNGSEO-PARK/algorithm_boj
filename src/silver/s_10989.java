d8371e0372b8d331db66cd8eee16c58f3d69432f
YOUNGSEO-PARK <irene0seo97@gmail.com>
2020-12-24 21:38:02 +0900

    [class 2] #10989 수 정렬하기3 solved

---
+++ src/silver/s_10989.java
@@ -0,0 +1,32 @@
+package silver;
+
+import java.io.*;
+
+// 수 정렬하기3
+public class s_10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] cnt = new int[10001];
+        int[] b = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+            cnt[a[i]]++;
+        }
+        for (int j = 1; j < cnt.length - 1; j++) {
+            cnt[j + 1] += cnt[j];
+        }
+        for (int k = n - 1; k > -1; k--) {
+            b[cnt[a[k]] - 1] = a[k];
+            cnt[a[k]]--;
+        }
+        for (int i : b) {
+            bw.write(i + "\n");
+        }
+        bw.close();
+    }
+}
