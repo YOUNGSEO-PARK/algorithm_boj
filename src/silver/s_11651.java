2fd91a381238fc08f6eb5c0c592fdffcd3b4f949
YOUNGSEO-PARK <irene0seo97@gmail.com>
2020-12-31 22:52:23 +0900

    [class 2] #11651 좌표 정렬하기 2 solved

--- src/silver/s_11651.java
+++ src/silver/s_11651.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 // 좌표 정렬하기 2
 public class s_11651 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
@@ -17,11 +17,11 @@ public class s_11651 {
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, Comparator.comparingInt(o->o[0]));
-        Arrays.sort(arr, Comparator.comparingInt(o->o[1]));
+        Arrays.sort(arr, Comparator.comparingInt(o -> o[0]));
+        Arrays.sort(arr, Comparator.comparingInt(o -> o[1]));
 
         for (int i = 0; i < n; i++) {
-            bw.write(arr[i][0] + " " + arr[i][1]+"\n");
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
         }
         bw.close();
     }
