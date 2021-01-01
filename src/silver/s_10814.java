f35abbec2b289e0fb6f53267a042367a24554fba
YOUNGSEO-PARK <irene0seo97@gmail.com>
2021-01-02 08:44:17 +0900

    [class 1] #10814 나이순 정렬 - Comparator() solved

--- src/silver/s_10814.java
+++ src/silver/s_10814.java
@@ -3,16 +3,15 @@ package silver;
 import java.io.*;
 import java.util.*;
 
-// 나이순 정렬 - Comparator
+// 나이순 정렬 - Comparator()
 public class s_10814 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String[][] strings = new String[n][2];
 
+        String[][] strings = new String[n][2];
         for(int i = 0; i < n ; i++){
-            String s = br.readLine();
-            StringTokenizer st = new StringTokenizer(s, " ");
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             strings[i][0] = st.nextToken();
             strings[i][1] = st.nextToken();
         }
@@ -23,8 +22,10 @@ public class s_10814 {
         // 내림차순을 위한 o1, o2를 교환한 람다식
         // Arrays.sort(strings, (o2, o1) -> Integer.compare(Integer.parseInt(o1[0]), Integer.parseInt(o2[0])));
 
+        StringBuilder sb = new StringBuilder();
         for (String[] s : strings){
-            System.out.println(s[0]+" "+s[1]);
+            sb.append(s[0]).append(" ").append(s[1]).append("\n");
         }
+        System.out.print(sb);
     }
 }
