aa062bdfc81ee7242557c357e24a4a5f8baddf22
YOUNGSEO-PARK <irene0seo97@gmail.com>
2021-01-05 00:43:00 +0900

    [class 2] #11866 요세푸스 문제 0 - 큐 solved

---
+++ src/silver/s_11866.java
@@ -0,0 +1,32 @@
+package silver;
+
+import java.io.*;
+import java.util.*;
+
+// 요세푸스 문제 0 - 큐
+public class s_11866 {
+    public static void main(String[] args) throws IOException {
+        Queue<Integer> queue = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            queue.offer(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while (!queue.isEmpty()) {
+            int cnt = k;
+            for (int j = cnt; cnt > 1; cnt--) {
+                queue.offer(queue.peek());
+                queue.remove();
+            }
+            sb.append(queue.poll()).append(", ");
+        }
+        sb.delete(sb.length() - 2, sb.length()).append(">");
+        System.out.print(sb);
+    }
+}
